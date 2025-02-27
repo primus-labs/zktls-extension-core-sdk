0037fd902ccdd97026450b049f0c81a84a8f9f60
fksyuan <fksyuan@163.com>
2025-02-27 17:54:06 +0800

    implement getAttestation and fix init

--- src/algorithm/primus_zk.js
+++ src/algorithm/primus_zk.js
@@ -1,6 +1,6 @@
 // load wasm module
+Module = {};
 Module_callAlgorithm = null;
-const Module = require("./client_plugin.js");
 Module.onRuntimeInitialized = async () => {
   console.log("Module onRuntimeInitialized");
   Module_callAlgorithm = Module.cwrap('callAlgorithm', 'string', ['string']);
@@ -9,7 +9,19 @@ Module.onRuntimeInitialized = async () => {
     resMethodName: 'start',
     res: 'RuntimeInitialized',
   });
-}
+};
+
+// Module_callAlgorithm = null;
+// const Module = require("./client_plugin.js");
+// Module.onRuntimeInitialized = async () => {
+//   console.log("Module onRuntimeInitialized");
+//   Module_callAlgorithm = Module.cwrap('callAlgorithm', 'string', ['string']);
+//   chrome.runtime.sendMessage({
+//     resType: 'algorithm',
+//     resMethodName: 'start',
+//     res: 'RuntimeInitialized',
+//   });
+// }
 
 const callAlgorithm = async (params) => {
   if (!Module_callAlgorithm) {
@@ -19,53 +31,37 @@ const callAlgorithm = async (params) => {
   return Module_callAlgorithm(params);
 };
 
-exports.init = async () => {
+init = async () => {
   const params = `{"method":"init","version":"1.1.1","params":{}}`;
   const result = await callAlgorithm(params);
-  return result;
+  return JSON.parse(result);
 };
 
-exports.getAttestation = async (paramsObj) => {
+getAttestation = async (paramsObj) => {
   const _paramsObj = { method: "getAttestation", version: "1.1.1", params: paramsObj };
   const params = JSON.stringify(_paramsObj);
   const result = await callAlgorithm(params);
   return JSON.parse(result);
 };
 
-exports.getAttestationResult = async (timeout = 2 * 60 * 1000) => {
+getAttestationResult = async () => {
   const params = `{"method":"getAttestationResult","version":"1.1.1","params":{"requestid":"1"}}`;
-
-  return new Promise((resolve, reject) => {
-    const start = performance.now();
-    const tick = async () => {
-      const timeGap = performance.now() - start;
-      let resObj = null;
-      try {
-        const res = await callAlgorithm(params);
-        resObj = JSON.parse(res);
-      } catch (err) {
-      }
-
-      if (resObj && (resObj.retcode == "0" || resObj.retcode == "2")) {
-        resolve(resObj);
-      } else if (timeGap > timeout) {
-        reject({
-          code: 'timeout',
-          data: resObj
-        });
-      } else {
-        setTimeout(tick, 1000);
-      }
-    };
-    tick();
-  });
+  const result = await callAlgorithm(params);
+  return JSON.parse(result);
 }
+
+startOffline = async (paramsObj) => {
+  const _paramsObj = { method: "startOffline", version: "1.1.1", params: paramsObj };
+  const params = JSON.stringify(_paramsObj);
+  const result = await callAlgorithm(params);
+  return JSON.parse(result);
+};
 
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
   console.log('offscreen onMessage message', message);
   let result;
   if (message.type === 'algorithm' && message.method === 'init') {
-    result = await init(message.params);
+    result = await init();
   } else if (
     message.type === 'algorithm' &&
     message.method === 'getAttestation'
@@ -82,6 +78,7 @@ chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
   ) {
     result = await startOffline(message.params);
   }
+  console.log('offscreen onMessage send result', result);
   sendResponse({ result });
 });
 
--- src/call_primus_zk.js
+++ src/call_primus_zk.js
@@ -8,12 +8,53 @@ exports.init = async () => {
 };
 
 exports.getAttestation = async (paramsObj) => {
-  return {retcode:"0"};
+  let res = await sendMessageAsync({
+    type: 'algorithm',
+    method: 'getAttestation',
+    params: paramsObj
+  });
+  return res.result;
 };
 
 exports.getAttestationResult = async (timeout = 2 * 60 * 1000) => {
-  return {retcode:"0"};
+  return new Promise((resolve, reject) => {
+    const start = performance.now();
+    const tick = async () => {
+      const timeGap = performance.now() - start;
+      let resObj = null;
+      try {
+        let res = await sendMessageAsync({
+          type: 'algorithm',
+          method: 'getAttestation',
+          params: paramsObj
+        });
+        resObj = res.result;
+      } catch (err) {
+      }
+
+      if (resObj && (resObj.retcode == "0" || resObj.retcode == "2")) {
+        resolve(resObj);
+      } else if (timeGap > timeout) {
+        reject({
+          code: 'timeout',
+          data: resObj
+        });
+      } else {
+        setTimeout(tick, 1000);
+      }
+    };
+    tick();
+  });
 }
+
+exports.startOffline = async (paramsObj) => {
+  let res = await sendMessageAsync({
+    type: 'algorithm',
+    method: 'startOffline',
+    params: paramsObj
+  });
+  return res.result;
+};
 
 function sendMessageAsync(message) {
   return new Promise((resolve, reject) => {
