1b254eb7477325f24836c234df915342ba7b6e2a
ysm <ysm@ysm.com>
2026-02-24 17:31:01 +0800

    chores: Remove unnecessary information

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,8 +3,8 @@ import { PrimusCoreTLS} from '../src/index';
 // describe('listData function', () => {
 //     // jest.setTimeout(50000);
     
-//     const appId = "0x899dd126268e3010beaa1ac141a2a0aa98deba09";
-//     const appSecret = "0x7da5d1cd2fdd494aa1176031151a6202734e30ddb14fd01dc3376616408ee0a7";
+//     const appId = "YOUR_APPID";
+//     const appSecret = "YOUR_APPSECRET";
 //     it('init', async () => {
 //         const zkTLS = new PrimusCoreTLS();
 //         const result = await zkTLS.init(appId, appSecret);
@@ -17,11 +17,8 @@ import { PrimusCoreTLS} from '../src/index';
 describe('test', () => {
     jest.setTimeout(50000);
     // production
-    const appId = "0xe319e567f70e2b2a153cb6ceaa73893648cde180";
-    const appSecret = "0x4348563b2178adc171d851bcc27054d7879e07a41263ccfaa3b00d63d056559a";
-    // test
-    // const appId = "0x899dd126268e3010beaa1ac141a2a0aa98deba09";
-    // const appSecret = "0x7da5d1cd2fdd494aa1176031151a6202734e30ddb14fd01dc3376616408ee0a7";
+    const appId = "YOUR_APPID";
+    const appSecret = "YOUR_APPSECRET";
     it('generate', async () => {
         console.log('--------------process.env', process.env.NODE_ENV)
         try {
